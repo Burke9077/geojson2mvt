4a097478bb7df277dbd0a7536a65140c636bbc34
Matt Burke <matt.burke@weatheranalytics.com>
2017-03-23 15:30:46 -0400

    Added child process to improve performance

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ const vtpbf = require('vt-pbf');
 const geojsonvt = require('geojson-vt');
 
 const helpers = require('./helpers.js');
+const child_process = require('child_process');
+const scribe = child_process.fork(__dirname + '/scribe.js');
 
-var writeTile = function(xPath, y, mvt) {
-    fs.writeFile(`${xPath}/${y}.mvt`, mvt, function(err) {
-        // File is done writing when this is hit, update the entry in the tracking entry
-        console.log(`Tile ${xPath}/${y} write completed`);
-        if (err) {
-            console.log(err);
+// Variable to keep track of what we've done so far
+var tileVars = {
+    total: 0,
+    remaining: 0,
+    done: false
+}
+
+// Log any messages sent from scribe
+scribe.on('message', function(m) {
+    // Decrement the counter, scribe is done
+    tileVars.remaining = tileVars.remaining - 1;
+
+    if (tileVars.done === true) {
+        if (tileVars.remaining === 0) {
+            process.exit(0);
         }
-    });
-};
+    }
+});
 
 var getTileForProcessing = function(z, x, y, tileIndex, options, xPath) {
     console.log(`Getting tile ${z} ${x} ${y} `);
@@ -24,7 +35,14 @@ var getTileForProcessing = function(z, x, y, tileIndex, options, xPath) {
         pbfOptions[options.layerName] = tile;
         var pbf = vtpbf.fromGeojsonVt(pbfOptions);
 
-        writeTile(xPath, y, pbf);
+        tileVars.total = tileVars.total + 1;
+        tileVars.remaining = tileVars.remaining + 1;
+
+        scribe.send({
+            xPath: xPath,
+            y: y,
+            pbf: pbf
+        });
     } else {
         console.log(`Tile ${xPath}/${y} empty. Not written.`);
     }
@@ -34,25 +52,13 @@ var geojson2mvt = function(geoJson, options, geojsonvtOptions) {
 
     const tileIndex = geojsonvt(geoJson, geojsonvtOptions);
 
-    // create the "root directory" to place downloaded tiles in
-    try {
-        fs.mkdirSync(options.rootDir, 0777);
-    } catch (err) {
-        if (err.code !== 'EEXIST') callback(err);
-    }
-
     var tileCoords = {};
     var tileBounds;
 
     for (var z = options.zoom.min; z <= options.zoom.max; z++) {
 
-        //create z directory in the root directory
+        // Starting directory (z)
         var zPath = `${options.rootDir}/${z.toString()}/`;
-        try {
-            fs.mkdirSync(zPath, 0777)
-        } catch (err) {
-            if (err.code !== 'EEXIST') callback(err);
-        }
 
         // get the x and y bounds for the current zoom level
         var tileBounds = helpers.getTileBounds(options.bbox, z);
@@ -61,13 +67,8 @@ var geojson2mvt = function(geoJson, options, geojsonvtOptions) {
         // x loop
         for (var x = tileBounds.xMin; x <= tileBounds.xMax; x++) {
 
-            // create x directory in the z directory
+            // X directory path
             var xPath = zPath + x.toString();
-            try {
-                fs.mkdirSync(xPath, 0777)
-            } catch (err) {
-                if (err.code !== 'EEXIST') callback(err);
-            }
 
             // y loop
             for (var y = tileBounds.yMin; y <= tileBounds.yMax; y++) {
@@ -75,6 +76,8 @@ var geojson2mvt = function(geoJson, options, geojsonvtOptions) {
             }
         }
     }
+
+    tileVars.done = true;
 };
 
 module.exports = geojson2mvt;
---
+++ src/scribe.js
@@ -0,0 +1,25 @@
+// Scribe writes things sent to it
+
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const getDirName = require('path').dirname;
+
+process.on('message', function(writeObjs) {
+    const path = `${writeObjs.xPath}/${writeObjs.y}.mvt`;
+    mkdirp(getDirName(path), function(err1) {
+        if (err1) {
+            console.error(err1);
+        }
+
+        console.log(`Begin writing ${path}`);
+        fs.writeFile(path, Buffer(writeObjs.pbf), function(err) {
+            if (err) {
+                console.error(err);
+            }
+
+            // File is done writing when this is hit, update the entry in the tracking entry
+            console.log(`Done writing ${path}`);
+            process.send(`Tile ${path} write completed`);
+        });
+    });
+});
